473e2a4443b30ab29b855e6f6576f38113777141
slafniy <slafniy@gmail.com>
2020-04-11 18:37:59 +0400

    initial commit

---
+++ ProtoMessageBench/Bench.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using ProtoMessageOriginal;
+
+namespace ProtoMessageBench
+{
+    internal static class Bench
+    {
+        public static string ProtoAsText =
+            "root_message {\n"
+            + "  single_int: 123\n"
+            + "  single_double: 123.2342425\n"
+            + "  single_submessage {\n"
+            + "    single_string: \"some string value\"\n"
+            + "    repeated_int: 638427648\n"
+            + "    repeated_int: 63842723138\n"
+            + "  }\n"
+            + "  repeated_submessage {\n"
+            + "    submessage_single_int: 21231231\n"
+            + "  }\n"
+            + "  repeated_submessage {\n"
+            + "    submessage_single_int: 2321543\n"
+            + "  }\n"
+            + "  repeated_submessage {\n"
+            + "    submessage_single_int: 4324\n"
+            + "  }\n"
+            + "  repeated_submessage {\n"
+            + "    submessage_single_int: 243553321543\n"
+            + "  }\n"
+            + "  repeated_submessage {\n"
+            + "    submessage_single_int: 6544\n"
+            + "  }\n"
+            + "  repeated_submessage {\n"
+            + "    submessage_single_int: 456\n"
+            + "  }\n"
+            + "  repeated_submessage {\n"
+            + "    submessage_single_int: 45644\n"
+            + "    submessage_single_int2: 45644\n"
+            + "    submessage_single_int3: 45644\n"
+            + "    submessage_single_int4: 45644\n"
+            + "    submessage_single_int5: 45644\n"
+            + "    submessage_single_int6: 45644\n"
+            + "    submessage_single_int7: 45644\n"
+            + "    submessage_single_int8: 45644\n"
+            + "    submessage_single_int9: 45644\n"
+            + "    submessage_single_int10: 45644\n"
+            + "    submessage_single_int11: 45644\n"
+            + "    submessage_single_int12: 45644\n"
+            + "    submessage_single_int13: 45644\n"
+            + "  }\n"
+            + "  repeated_submessage {\n"
+            + "    submessage_single_int: 45644\n"
+            + "    submessage_single_int2: 45644\n"
+            + "    submessage_single_int3: 45644\n"
+            + "    submessage_single_int4: 45644\n"
+            + "    submessage_single_int5: 45644\n"
+            + "    submessage_single_int6: 45644\n"
+            + "    submessage_single_int7: 45644\n"
+            + "    submessage_single_int8: 45644\n"
+            + "    submessage_single_int9: 45644\n"
+            + "    submessage_single_int10: 45644\n"
+            + "    submessage_single_int11: 45644\n"
+            + "    submessage_single_int12: 45644\n"
+            + "    submessage_single_int13: 45644\n"
+            + "  }\n"
+            + "  repeated_submessage {\n"
+            + "    submessage_single_int: 45644\n"
+            + "    submessage_single_int2: 45644\n"
+            + "    submessage_single_int3: 45644\n"
+            + "    submessage_single_int4: 45644\n"
+            + "    submessage_single_int5: 45644\n"
+            + "    submessage_single_int6: 45644\n"
+            + "    submessage_single_int7: 45644\n"
+            + "    submessage_single_int8: 45644\n"
+            + "    submessage_single_int9: 45644\n"
+            + "    submessage_single_int10: 45644\n"
+            + "    submessage_single_int11: 45644\n"
+            + "    submessage_single_int12: 45644\n"
+            + "    submessage_single_int13: 45644\n"
+            + "  }\n"
+            + "}";
+
+        public static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (uint i = 0; i < uint.Parse(args[0]); i++)
+            {
+                var pm = new ProtoMessage();
+                pm.Parse(ProtoAsText);
+
+                pm.GetKeys();
+                ProtoMessage root = pm.GetElement("root_message");
+                var intAttr = root.GetAttribute<int>("single_int");
+                double? doubleAttr = root.GetAttributeOrNull<double>("single_double");
+                ProtoMessage singleMsg = root.GetElement("single_submessage");
+                List<string> repeatedInt = singleMsg.GetAttributeList("repeated_int");
+                List<ProtoMessage> repeatedSubMsg = root.GetElementList("repeated_submessage");
+                var subMsgInt = repeatedSubMsg[1].GetAttribute<ulong>("submessage_single_int");
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"{args[0]} iterations finished in {stopwatch.Elapsed.TotalSeconds} sec");
+        }
+    }
+}
---
+++ ProtoMessageUT/Tests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using ProtoMessageOriginal;
+
+namespace ProtoMessageUT
+{
+    [TestFixture]
+    internal class ProtoBuffUt
+    {
+        private const string ContractId = "45";
+        private const string FirstScaledPrice = "5883";
+        private const string SecondScaledPrice = "14";
+
+        private readonly string _protoText =
+            "real_time_market_data {\n"
+            + $"  contract_id: {ContractId}\n"
+            + "  quote {\n"
+            + "    type: 1\n"
+            + "    quote_utc_time: 638427648\n"
+            + $"    scaled_price: {FirstScaledPrice}\n"
+            + $"    scaled_price: {SecondScaledPrice}\n"
+            + "  }\n"
+            + "  quote {\n"
+            + "    type: 2\n"
+            + $"    scaled_price: {FirstScaledPrice}\n"
+            + $"    scaled_price: {SecondScaledPrice}\n"
+            + "  }\n"
+            + "}";
+
+        [Test]
+        public void ProtoParse()
+        {
+            var pm = new ProtoMessage();
+            pm.Parse(_protoText);
+            pm = pm.GetElement("real_time_market_data");
+            var listPm = pm.GetElementList("quote");
+            Assert.IsNotNull(pm);
+            Assert.That(pm.GetAttribute("contract_id"), Is.EqualTo(ContractId));
+            Assert.That(listPm.Count, Is.EqualTo(2));
+            foreach (var protoMessage in listPm)
+            {
+                var attrList = protoMessage.GetAttributeList("scaled_price");
+                Assert.That(protoMessage.GetAttribute("scaled_price"), Is.EqualTo(FirstScaledPrice));
+                Assert.That(attrList.Count, Is.EqualTo(2));
+                Assert.That(attrList[1], Is.EqualTo(SecondScaledPrice));
+            }
+
+            Assert.AreEqual(new List<string> {"quote", "quote"}, pm.GetKeys());
+        }
+    }
+}
