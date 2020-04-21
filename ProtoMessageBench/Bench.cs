9ef42841ca16c3ebcde49daa6c89057d9f9cdc59
slafniy <slafniy@gmail.com>
2020-04-22 01:44:51 +0400

    Changes in Bench - added second argument in order to run only one ProtoMessage type bench, because results are position related

--- ProtoMessageBench/Bench.cs
+++ ProtoMessageBench/Bench.cs
@@ -11,23 +11,22 @@ namespace ProtoMessageBench
 {
     internal static class Bench
     {
-
         private static void JustParse<T>(uint iterations, string protoAsText) where T : IProtoMessage<T>, new()
         {
             var stopwatch = new Stopwatch();
             stopwatch.Start();
-            
+
             for (uint i = 0; i < iterations; i++)
             {
                 var protoMessage = new T();
                 protoMessage.Parse(protoAsText);
             }
-            
+
             stopwatch.Stop();
             Console.WriteLine($"{iterations} iterations JustParse for {typeof(T)} " +
                               $"finished in {stopwatch.Elapsed.TotalSeconds} sec");
         }
-        
+
         private static void ReadAll<T>(uint iterations, string protoAsText) where T : IProtoMessage<T>, new()
         {
             var stopwatch = new Stopwatch();
@@ -61,7 +60,7 @@ namespace ProtoMessageBench
                 lvlTwoMsg.GetAttribute("write_here");
                 string lvlOneBool = lvlOneMsg.GetAttribute("and_boolean_in_the_end");
                 string? needToWriteAttr = null;
-                
+
                 foreach (T msg in lvlThreeMessages)
                 {
                     needToWriteAttr = msg.GetAttribute("need_to_write_something_here");
@@ -75,13 +74,13 @@ namespace ProtoMessageBench
                 T uniqueOtherL3 = lvlTwoMsg.GetElement("message_level_three_but_unique");
                 string key = uniqueOtherL3.GetAttribute("key");
                 string text = uniqueOtherL3.GetAttribute("text");
-                
+
                 // made a check once per 1000 iterations just to make sure everything is correct
                 if (i % 1000 != 0)
                 {
                     continue;
                 }
-                
+
                 Assert.AreEqual(13, keys.Count);
                 Assert.IsNotEmpty(lvlThreeMessages);
                 Assert.AreEqual(28, zeroIds.Count);
@@ -96,7 +95,7 @@ namespace ProtoMessageBench
             }
 
             stopwatch.Stop();
-            Thread.Sleep(100);  // TODO: fix. Console in Rider goes mad without this
+            Thread.Sleep(100); // TODO: fix. Console in Rider goes mad without this
             Console.WriteLine($"{iterations} iterations ReadAll for {typeof(T)} " +
                               $"finished in {stopwatch.Elapsed.TotalSeconds} sec");
             Thread.Sleep(100);
@@ -106,7 +105,7 @@ namespace ProtoMessageBench
         {
             var stopwatch = new Stopwatch();
             stopwatch.Start();
-            
+
             for (uint i = 0; i < iterations; i++)
             {
                 var protoMessage = new T();
@@ -117,41 +116,51 @@ namespace ProtoMessageBench
                 T msgLevelTwo = msgLevelZero.GetElement("message_level_one").GetElement("message_level_two");
                 string uselessId = msgLevelTwo.GetAttribute("useless_id");
                 string longString = msgLevelTwo.GetAttribute("long_string_attribute");
-                
+
                 // made a check once per 1000 iterations just to make sure everything is correct
                 if (i % 1000 != 0)
                 {
                     continue;
                 }
-                
+
                 Assert.AreEqual("0", intId);
                 Assert.AreEqual("254", uselessId);
                 Assert.AreEqual("2387o497ghl233j rl dj nf we \"f\"jp w3" +
                                 "8f p!@#$ @#$% @!~@!#$@Ss42``42344 2 34234p909k09fjd", longString);
             }
-            
+
             stopwatch.Stop();
             Console.WriteLine($"{iterations} iterations PartialRead for {typeof(T)} " +
                               $"finished in {stopwatch.Elapsed.TotalSeconds} sec");
         }
-        
+
         public static void Main(string[] args)
         {
-            Stream dataStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ProtoMessageBench.TestData.txt");
+            Stream dataStream = Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream("ProtoMessageBench.TestData.txt");
             var dataReader = new StreamReader(dataStream ?? throw new Exception("Cannot read data"));
             string testData = dataReader.ReadToEnd();
 
-            // JustParse<ProtoMessage>(uint.Parse(args[0]), testData);
-            JustParse<ProtoMessage2>(uint.Parse(args[0]), testData);
-            JustParse<ProtoMessage4>(uint.Parse(args[0]), testData);
-            
-            // PartialRead<ProtoMessage>(uint.Parse(args[0]), testData);
-            PartialRead<ProtoMessage2>(uint.Parse(args[0]), testData);
-            PartialRead<ProtoMessage4>(uint.Parse(args[0]), testData);
-            
-            // ReadAll<ProtoMessage>(uint.Parse(args[0]), testData);
-            ReadAll<ProtoMessage2>(uint.Parse(args[0]), testData);
-            ReadAll<ProtoMessage4>(uint.Parse(args[0]), testData);
+            switch (args.Length > 1 ? args[1] : "")
+            {
+                case "0":
+                    JustParse<ProtoMessage>(uint.Parse(args[0]), testData);
+                    PartialRead<ProtoMessage>(uint.Parse(args[0]), testData);
+                    ReadAll<ProtoMessage>(uint.Parse(args[0]), testData);
+                    break;
+                case "2":
+                    JustParse<ProtoMessage2>(uint.Parse(args[0]), testData);
+                    PartialRead<ProtoMessage2>(uint.Parse(args[0]), testData);
+                    ReadAll<ProtoMessage2>(uint.Parse(args[0]), testData);
+                    break;
+                case "4":
+                    JustParse<ProtoMessage4>(uint.Parse(args[0]), testData);
+                    PartialRead<ProtoMessage4>(uint.Parse(args[0]), testData);
+                    ReadAll<ProtoMessage4>(uint.Parse(args[0]), testData);
+                    break;
+                default:
+                    throw new Exception("Provide ProtoMessage number - 0, 2 or 4 in second argument");
+            }
         }
     }
 }
