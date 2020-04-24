779e5094406149b1bc35e9578ca45f9be3df7b5d
slafniy <slafniy@gmail.com>
2020-04-24 20:44:29 +0400

    comments for IProtoMessage

--- ProtoMessage/IProtoMessage.cs
+++ ProtoMessage/IProtoMessage.cs
@@ -4,14 +4,14 @@ namespace ProtoMessage
 {
     public interface IProtoMessage<TType> where TType : IProtoMessage<TType>, new()
     {
-        List<TType> GetElementList(string name);
-        TType GetElement(string name);
-        List<string> GetAttributeList(string name);
-        T GetAttribute<T>(string name) where T : struct;
+        List<TType> GetElementList(string name);  // to get repeated messages
+        TType GetElement(string name);  // to get optional/required message or first message from repeated
+        List<string> GetAttributeList(string name);  // to get repeated attribute values
+        T GetAttribute<T>(string name) where T : struct;  
         T? GetAttributeOrNull<T>(string name) where T : struct;
-        string GetAttribute(string name);
+        string GetAttribute(string name);  // to get attribute value
         void Parse(string text);
         bool HasKey(string name);
-        List<string> Keys { get; }
+        List<string> Keys { get; }  // return inner message names (without recursion!)
     }
 }
